35a327ee1d49ab04ec7c3c11f3c6c3fe8e1440cd
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-01-16 22:00:43 -0500

    added pagination query fields in query resolvers

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,31 +1,44 @@
-// Author: Austin Howlett
-// Description: Responsible for resolving all query (in relation to REST this would GET endpoints) schema endpoints (business logic)
-
 const { verifyToken } = require('../utils')
 const { communityFragment } = require('../fragments/communityFragment')
 
+// TODO: Review permissions for these... for now full access
 communityById = (_, args, context, info) => {
-  const payload = verifyToken(context)
+  //const payload = verifyToken(context)
   return context.prisma.community({ id: args.id })
     .$fragment(communityFragment)
 }
 
 communityByName = (_, args, context, info) => {
-  const payload = verifyToken(context)
+  //const payload = verifyToken(context)
   return context.prisma.communities({ where: { name: args.name } })
     .$fragment(communityFragment)
 }
 
 communities = (_, args, context, info) => {
-  const payload = verifyToken(context)
-  return context.prisma.communities()
-    .$fragment(communityFragment)
+  //const payload = verifyToken(context)
+  return context.prisma.communities({
+    where: {
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip
+    }
+  }).$fragment(communityFragment)
 }
 
 membersCommunities = (_, args, context, info) => {
-  const payload = verifyToken(context)
-  return context.prisma.communities({ where: { id_in: args.communitiesIds }})
-    .$fragment(communityFragment)
+  //const payload = verifyToken(context)
+  return context.prisma.communities({
+    where: {
+      id_in: args.communitiesIds,
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip
+    }
+  }).$fragment(communityFragment)
 }
 
 module.exports = {
